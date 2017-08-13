202d2f56d71780f11dcd3d06e2e5f306c59eda97
Matt Eves <mattheweves@outlook.com>
2017-08-13 07:51:58 -0700

    Image Processing

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
   # Choose what kind of storage to use for this uploader:
   #storage :file
   storage :fog
@@ -42,8 +42,8 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  #def filename
+  #   "accountimage" + DateTime.now.strftime("%Y%m%d%H%M%S") + ".jpg"
+  #end
 
 end
